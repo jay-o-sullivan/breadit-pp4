de65cade78d47eb6df4debb05c650e46e2d8efee
jay-o-sullivan <jayosullivan25@gmail.com>
2024-05-17 02:08:24 +0000

    add  commenting part 1

--- breadit/settings.py
+++ breadit/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'crispy_forms',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
