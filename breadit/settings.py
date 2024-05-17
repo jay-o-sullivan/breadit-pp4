a62be37159cf19a8a63f0c7d542c2f237e6c0c73
jay-o-sullivan <jayosullivan25@gmail.com>
2024-05-17 15:54:12 +0000

    deployment commit

--- breadit/settings.py
+++ breadit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
@@ -30,9 +31,12 @@ TEMPLATES_DIR =  os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-jayosullivan-breaditpp4-5phf5gdb7kc.ws-eu111.gitpod.io', 'breadit-9c8dede03c46.herokuapp.com']
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
+ALLOWED_HOSTS = ['8000-jayosullivan-breaditpp4-5phf5gdb7kc.ws-eu114.gitpod.io', 'breadit-9c8dede03c46.herokuapp.com']
 
 
 # Application definition
@@ -59,6 +63,16 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+    }
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
